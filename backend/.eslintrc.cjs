43a8dbe279dc5b20008acf5db118f0bfbb4ff41a
HoodProject <97952497+HoodProject@users.noreply.github.com>
2025-10-02 15:42:53 +0200

    Add kiosk-style lock screen script

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+    sourceType: 'module'
+  },
+  env: {
+    node: true,
+    es2020: true
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-misused-promises': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  }
+};
